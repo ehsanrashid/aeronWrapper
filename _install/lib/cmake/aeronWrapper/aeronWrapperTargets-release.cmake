b71a07e976ab8bde8845b42a7d545032ad256726
Huzaifa309 <ahmedhuzaifa247@gmail.com>
2025-08-28 12:18:48 +0500

    check 1

--- _install/lib/cmake/aeronWrapper/aeronWrapperTargets-release.cmake
+++ _install/lib/cmake/aeronWrapper/aeronWrapperTargets-release.cmake
@@ -15,25 +15,5 @@ set_target_properties(aeronWrapper::aeronWrapper PROPERTIES
 list(APPEND _cmake_import_check_targets aeronWrapper::aeronWrapper )
 list(APPEND _cmake_import_check_files_for_aeronWrapper::aeronWrapper "${_IMPORT_PREFIX}/lib/libaeronWrapper.a" )
 
-# Import target "aeronWrapper::aeron_client" for configuration "Release"
-set_property(TARGET aeronWrapper::aeron_client APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(aeronWrapper::aeron_client PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libaeron_client.a"
-  )
-
-list(APPEND _cmake_import_check_targets aeronWrapper::aeron_client )
-list(APPEND _cmake_import_check_files_for_aeronWrapper::aeron_client "${_IMPORT_PREFIX}/lib/libaeron_client.a" )
-
-# Import target "aeronWrapper::aeron_driver" for configuration "Release"
-set_property(TARGET aeronWrapper::aeron_driver APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(aeronWrapper::aeron_driver PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libaeron_driver.so"
-  IMPORTED_SONAME_RELEASE "libaeron_driver.so"
-  )
-
-list(APPEND _cmake_import_check_targets aeronWrapper::aeron_driver )
-list(APPEND _cmake_import_check_files_for_aeronWrapper::aeron_driver "${_IMPORT_PREFIX}/lib/libaeron_driver.so" )
-
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
